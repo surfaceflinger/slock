c85f23533ce43188e1a5bc76bd4cc26f3b766e6a
Morelcia <nat@nekopon.pl>
2022-01-05 21:02:36 +0100

    XFCE4 power manager so so bad : (

--- slock.c
+++ slock.c
@@ -383,7 +383,7 @@ main(int argc, char **argv) {
 		die("slock: DPMSEnable failed\n");
 	if (!DPMSGetTimeouts(dpy, &standby, &suspend, &off))
 		die("slock: DPMSGetTimeouts failed\n");
-	if (!standby || !suspend || !off)
+	if (!standby || !off)
 		die("slock: at least one DPMS variable is zero\n");
 	if (!DPMSSetTimeouts(dpy, monitortime, monitortime, monitortime))
 		die("slock: DPMSSetTimeouts failed\n");
